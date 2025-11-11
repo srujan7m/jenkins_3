3524cd61d64adea4c904b2eaf5db72a6686ae2db
srujan7m <srujanmmattur@gmail.com>
2025-11-11 09:58:32 +0530

    temp

---
+++ temperature.py
@@ -0,0 +1,14 @@
+import sys
+
+if len(sys.argv) >= 2:
+    script_name = sys.argv[0]
+    temp = sys.argv[1]
+else:
+    temp = 25.0
+
+if temp < 15:
+    print("cold")
+elif temp <= 30:
+    print("normal")
+else:
+    print("hot")
